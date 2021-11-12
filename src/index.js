bbb9cad0aac64ef8ab5de4ba6c2a85db1e6fc037
Sowad Mim <sowad1035@gmail.com>
2021-11-12 16:57:56 +0600

    setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    supportedLngs: ['en', 'ar', 'bn'],
+    detection: {
+      order: [
+        'path',
+        'cookie',
+        'localStorage',
+        'navigator',
+        'subdomain',
+        'htmlTag',
+      ],
+      caches: ['cookie'],
+    },
+    fallbackLng: 'en',
+    backend: {
+      loadPath: '/locales/{{lng}}/translation.json',
+    },
+    react: { useSuspense: false },
+  });
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +37,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
